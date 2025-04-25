92e10e31543145e27e0f89ed7e866e74dead67bc
2204948 <146948699+2204948@users.noreply.github.com>
2025-04-25 22:33:10 +0100

    Update ate tarefas 27-04
    Criei os prototipos de model e controller, atualizei as pastas e organizei.

---
+++ Prototipos/Controller/GameController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class GameController
+{
+    private GameModel model;
+    private GameView view;
+
+    private float lastShotTime = -1f;
+    private const float bulletCooldown = 0.5f;
+
+    public GameController(GameView view)
+    {
+        this.view = view;
+        this.model = new GameModel();
+
+        // Ligar eventos do Model à View
+        model.OnPositionChanged += view.UpdatePlayerPosition;
+        model.BulletFired += view.SpawnBullet;
+        model.BulletMoved += view.MoveBullet;
+        model.BulletDestroyed += view.DestroyBullet;
+        model.OnEnemySpawn += view.SpawnEnemy;
+        model.OnEnemyKilled += view.ShowExplosion;
+        model.OnGameOver += view.ShowGameOver;
+
+        // Inicializar o jogo
+        model.StartNewGame();
+        view.UpdateScore(0);
+    }
+
+    public void OnPlayerInput(float direction, bool shoot)
+    {
+        model.Move(direction, Time.deltaTime);
+
+        if (shoot && Time.time - lastShotTime >= bulletCooldown)
+        {
+            lastShotTime = Time.time;
+            model.TryShot();
+        }
+    }
+
+    public void Update()
+    {
+        model.Update(Time.deltaTime);
+    }
+
+    public void OnEnemyKilled(Vector2 enemyPosition)
+    {
+        model.EnemyHit(enemyPosition);
+    }
+
+    public void SpawnEnemy(Vector2 position)
+    {
+        model.EnemySpawn(position);
+    }
+
+    public void RestartGame()
+    {
+        model.StartNewGame();
+        view.UpdateScore(0);
+        view.StartGame();
+    }
+}
---
+++ Prototipos/Model/GameModel.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+// Corrige para usar os tipos do Unity
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class GameModel
+{
+    // Eventos
+    public event Action<Vector3> OnPositionChanged;
+    public event Action<Vector3> BulletFired;
+    public event Action<Vector3> BulletMoved;
+    public event Action<Vector3> BulletDestroyed;
+    public event Action<Vector2> OnEnemySpawn;
+    public event Action<Vector2> OnEnemyKilled;
+    public event Action OnGameOver;
+
+    // Estado do jogador
+    private Vector3 playerPosition;
+
+    // Estado de balas e inimigos
+    private List<Vector3> bullets = new List<Vector3>();
+    private List<Vector2> enemies = new List<Vector2>();
+
+    // Outras variáveis
+    public const float playerMovSpeed = 8.0f;
+    public const float bulletSpeed = 10f;
+    private readonly float maxLeft = -14f;
+    private readonly float maxRight = 14f;
+    private readonly float bulletMaxY = 20f;
+
+    private int score = 0;
+
+    // Métodos
+
+    public void StartNewGame()
+    {
+        bullets.Clear();
+        enemies.Clear();
+        score = 0;
+        playerPosition = new Vector3(0f, -14f, 0f);
+        OnPositionChanged?.Invoke(playerPosition);
+    }
+
+    public void SetInitialPosition(Vector3 initialPosition)
+    {
+        playerPosition = initialPosition;
+        OnPositionChanged?.Invoke(playerPosition);
+    }
+
+    public void Move(float direction, float deltaTime)
+    {
+        float deltaX = direction * playerMovSpeed * deltaTime;
+        playerPosition.x = Mathf.Clamp(playerPosition.x + deltaX, maxLeft, maxRight);
+        OnPositionChanged?.Invoke(playerPosition);
+    }
+
+    public void TryShot()
+    {
+        Vector3 bulletPos = playerPosition + new Vector3(0f, 1f, 0f);
+        bullets.Add(bulletPos);
+        BulletFired?.Invoke(bulletPos);
+    }
+
+    public void Update(float deltaTime)
+    {
+        for (int i = bullets.Count - 1; i >= 0; i--)
+        {
+            bullets[i] += Vector3.up * bulletSpeed * deltaTime;
+
+            if (bullets[i].y > bulletMaxY)
+            {
+                BulletDestroyed?.Invoke(bullets[i]);
+                bullets.RemoveAt(i);
+            }
+            else
+            {
+                BulletMoved?.Invoke(bullets[i]);
+            }
+        }
+    }
+
+    public void EnemySpawn(Vector2 position)
+    {
+        enemies.Add(position);
+        OnEnemySpawn?.Invoke(position);
+    }
+
+    public void EnemyHit(Vector2 position)
+    {
+        if (enemies.Remove(position))
+        {
+            OnEnemyKilled?.Invoke(position);
+        }
+
+        if (GameOverCheck())
+        {
+            OnGameOver?.Invoke();
+        }
+    }
+
+    public bool GameOverCheck()
+    {
+        if (enemies.Count == 0)
+        {
+            return true;
+        }
+
+        foreach (var enemy in enemies)
+        {
+            if (enemy.y <= playerPosition.y)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
