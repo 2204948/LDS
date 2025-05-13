9f37db22315440390e3139391a9609ad24bb7e68
Fábio Silva <1fabiosilva1@gmail.com>
2025-05-13 22:26:00 +0100

    Update GameController.cs

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -1,13 +1,7 @@
-using Unity.VisualScripting;
-using UnityEngine.InputSystem.XR;
-
 public class GameController
 {
     private IGameModel model;
     private GameView view;
-
-    private float lastShotTime = -1f;
-    private const float bulletCooldown = 0.5f;
 
     public GameController(GameView view, GameModel model)
     {
@@ -19,7 +13,7 @@ public class GameController
         view.OnGameOverStart += StartGame;
         view.OnGameOverQuit += ShowMenu;
         view.ShowMainMenu();
-        
+
     }
 
     public void OnPlayerInput(float direction, float deltaTime)
@@ -34,12 +28,6 @@ public class GameController
     }
 
     public void StartGame()
-    {
-        model.StartNewGame();
-        view.StartGame();
-    }
-
-    public void RestartGame()
     {
         model.StartNewGame();
         view.StartGame();
